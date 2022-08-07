7124a1394572d246d9617c28ba229ffa8bb7a984
mattgoforit <mattgoforit@gmail.com>
2022-08-08 01:02:19 +0800

    试一试不加缓存直接提commit

--- src/algorithm/ATestDemo.java
+++ src/algorithm/ATestDemo.java
@@ -1,10 +1,13 @@
 package algorithm;
 
+
 import java.io.IOException;
 import java.util.*;
 //git测试
 public class ATestDemo {
     public static void main(String[] args) {
+        String string = new String();
+        String s = "";
 
     }
 }
